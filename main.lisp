f50b308fd92273d781c81c1f767b486f9819c8e6
Igor Jovanovic <igorjovanovic2898@gmail.com>
2020-12-20 03:42:39 +0100

    mala ispravka kod funkcije za stapanje

--- main.lisp
+++ main.lisp
@@ -104,7 +104,7 @@
         ;((and (equalp (+(floor ind n)(mod ind n )) hor )(equalp (floor ind n) ver ))(format t "~a " (nth ind tabla))(stampaj tabla hor (1+ ver) 0 (mod (1+ inkr) n)))
         ((> ver (- (* n n) 1 )) (format t "~%")(stampaj tabla (- hor 1) 0 0 0))
         ;((< ind (* n n n))(stampaj tabla hor ver (1+ ind) inkr))
-        (t (princ "- ") (stampaj tabla hor (1+ ver) 0 (mod(1+ inkr)n)))
+        ;(t (princ "- ") (stampaj tabla hor (1+ ver) 0 (mod(1+ inkr)n)))
         (t (format t"~a "(nth (+(* ver n)(- hor inkr)) tabla))(stampaj tabla hor(1+ ver)0(mod(1+ inkr)n)))
         )
   )
@@ -120,9 +120,9 @@
 )
 
 
-(st (car(nova-stanja tabela 'x)))
+(st (car(nova-stanja(car(nova-stanja tabela 'x)) 'o)))
 
-(setq n 6)
+(setq n 10)
 (setq tabela (Kreiraj_Tablu n))
 
 (untrace stampaj)
