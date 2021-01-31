549b536385c93fbb7df92a612b74fbce89f84324
Igor Jovanovic <igorjovanovic2898@gmail.com>
2021-01-31 02:47:11 +0100

    jaoo

--- main.lisp
+++ main.lisp
@@ -11,17 +11,17 @@
 (defun ima (potez i stanje)
   (equal (nth i stanje) potez))
 
-(defun sledbenici (pred sled potez)
+(defun sledbenici (pred sled potez slob i)
   (cond ((null sled) '())
-        ((equal (car sled) '-)
+        ((and (equal (car sled) '-) slob)
          (cons (append pred (list potez) (cdr sled))
                (sledbenici (append pred (list(car sled)))
-                           (cdr sled)  potez)))
+                           (cdr sled)  potez '() (if (equalp i 3) '0 (1+ i)) )))
          (t (sledbenici (append pred (list (car sled)))
-                        (cdr sled) potez))))
+                        (cdr sled) potez (if (equalp i 3) 't slob ) (if (equalp i 3) '0 (1+ i)) ))))
 
 (defun nova-stanja (stanje potez)
-  (sledbenici '() stanje  potez))
+  (sledbenici '() stanje  (if potez 'x 'o) 't '0))
 
 (defun stampaj-indekse (indeks n)
   (cond ((equal indeks n) '())
@@ -57,35 +57,46 @@
   (progn (format t "~%Unesite potez (vrsta kolona) ~a :" (if igrac 'X 'O))
     (let* ((potez (read))
            (oznaka (if igrac 'x 'o))
-           (izlaz (postavi stanje oznaka (+ (* n n (- (car potez) 1)) (* n(-(cadr potez) 1 ))) (- n 1) )))
-      (if (equal stanje izlaz) (unesi stanje igrac) izlaz))))
+           (val (if (or (null (car potez)) (null (cadr potez)) (not(atom (car potez))) (not(atom (cadr potez)))) (unesi stanje igrac))))
+              (if val (unesi stanje igrac)
+              	 (let* ((izlaz (postavi stanje oznaka (+ (* n n (- (car potez) 1)) (* n(-(cadr potez) 1 ))) (- n 1) ))) 
+              	  (if (or (equal stanje izlaz)   (< (car potez) 1) (> (car potez) 4)  (< (cadr potez) 1) (> (cadr potez) 4) ) (unesi stanje igrac) izlaz)) )
+           )
+   
+     ))
 
 (defun 4Connect()
-
   (format t "~5%Loading...R2D2(Connect 4 igrica)...~%~%")
     (format t "~%~%Unesite zeljenu dimenziju table ?~% (n mora da bude paran broj)~%> ")
   (setq n (read))
   (if (=(mod n 2)1)  (4Connect))
   (if(> n 6) (4connect))
-               (format t "~%~%Zelim da igram~%(X = 1 | O = 2)~%> ")
+  (format t "~%~%Zelim da igram~%(X = 1 | O = 2)~%> ")
   (setq play (read))
-              (cond ((equal play 1) (format t "~2%Izabrali ste da igrate prvi~%~%" ) (setq player1 T))
-                    ((equal play 2) (format t "~2%Izabrali ste da igrate drugi~%~%" ) (setq player1 '()))
-                    (t (format t "~2%Niste nista izbrali od navedenog~%~%" ) (4Connect)))
+  (cond ((equal play 1) (format t "~2%Izabrali ste da igrate X~%~%" ) (setq player1 T))
+        ((equal play 2) (format t "~2%Izabrali ste da igrate O~%~%" ) (setq player1 '()))
+        (t (format t "~2%Niste nista izbrali od navedenog~%~%" ) (4Connect)))
+  (format t "~%Unesite r ako racunar igra prvi (r ili c):~%> ")
+  (setq auto (read))
+  (cond ((equal play 'r) (format t "~2%Izabrali ste da prvi igra racunarX~%~%" ) (setq auto T))
+        ((equal play ç) (format t "~2%Izabrali ste da prvi igrate viO~%~%" ) (setq auto '()))
+        (t (format t "~2%Niste nista izbrali od navedenog~%~%" ) (4Connect)))
+  
   (igraj (Kreiraj_Tablu n) player1)
+  
   )
 
 
 
 (defun igraj(stanje igrac)
-  (let* ((nstanje (unesi stanje igrac)))
-    (progn (st nstanje))
-    (if (not (kraj nstanje))
-        (let* ((nnstanje (unesi nstanje (not igrac))))
-          (progn (st nnstanje))
-          (if (not (kraj nnstanje))
-          (igraj nnstanje igrac)(format t "~%~%Pobedio igrac ~a ~%>" (if igrac 'O 'X)))) (format t "~%~%Pobedio igrac ~a ~%>" (if igrac 'X 'O)))))
-
+  (let* ((nstanje (if igrac (unesi stanje igrac) (car (minmax stanje 3 igrac t))))
+         (progn (st nstanje))
+         (if (not (kraj nstanje))
+             (let* ((nnstanje (if igrac (car(minmax nstanje 4 (not igrac) t)) (unesi nstanje (not igrac)))))
+               (progn (st nnstanje))
+               (if (not (kraj nnstanje))
+                   (igraj nnstanje igrac)(format t "~%~%Pobedio igrac ~a ~%>" (if igrac 'O 'X)))) (format t "~%~%Pobedio igrac ~a ~%>" (if igrac 'X 'O))))))
+  
 
 
 (defun kraj (stanje)
@@ -195,5 +206,61 @@
           )
   )
 
+(defun max-stanje (lsv)
+  (max-stanje-i (cdr lsv)(car lsv))
+)
+
+(defun max-stanje-i (lsv stanje-vrednosti)
+(cond 
+  ((null lsv) stanje-vrednosti)
+  ((> (cadar lsv)(cadr stanje-vrednost))
+        (max-stanje-i (cdr lsv) (car lsv)))
+  (t (max-stanje-i (cdr lsv) stanje-vrednosti))
+))
+
+(defun min-stanje (lsv)
+  (min-stanje-i (cdr lsv)(car lsv))
+)
+
+(defun min-stanje-i (lsv stanje-vrednosti)
+(cond 
+  ((null lsv) stanje-vrednosti)
+  ((< (cadar lsv)(cadr stanje-vrednost))
+        (min-stanje-i (cdr lsv) (car lsv)))
+  (t (min-stanje-i (cdr lsv) stanje-vrednosti))
+))
+
+(defun minmax (stanje dubina moj-potez start)
+  (cond (( or (zerop dubina) (null lp)) (list stanje(or (kraj stanje) (proceni-stanje stanje))))
+        (t ((let 
+                ((lp (nova-stanja stanje moj-potez))
+                 (f (if moj-potez 'max-stanje 'min-stanje)))
+              (cond 
+               (start (apply f (list (mapcar (lambda (x) (minmax x (1- dubina) (not moj-potez) '())) lp))))
+               (t (cons stanje (cdr (apply f (list (mapcar (lambda (x) (minmax x (1- dubina)(not moj-potez) '())) lp))))))))))))
+
+              
+(defun proceni-stanje (stanje)
+ (proceni-hor stanje) )
+
+(defun proceni-hor (stanje)
+  (+ (if (and (ista 0 1 stanje) (ista 0 2 stanje) (ima 'x 0 stanje)) 1 0)
+     (if (and (ista 0 1 stanje) (ista 0 2 stanje) (ista 0 3 stanje) (ima 'x 0 stanje)) 3 0)
+     (if (and (ista 1 2 stanje) (ima 'x 1 stanje)) 1 0)
+     (if (and (ista 0 1 stanje) (ima 'o 0 stanje)) '-1 0)
+     (if (and (ista 0 2 stanje) (ima 'o 0 stanje)) '-1 0)
+     (if (and (ista 1 2 stanje) (ima 'o 1 stanje)) '-1 0)
+     (if (and (ista 3 4 stanje) (ima 'x 3 stanje)) 1 0)
+     (if (and (ista 3 5 stanje) (ima 'x 3 stanje)) 1 0)
+     (if (and (ista 4 5 stanje) (ima 'x 4 stanje)) 1 0)
+     (if (and (ista 3 4 stanje) (ima 'o 3 stanje)) '-1 0)
+     (if (and (ista 3 5 stanje) (ima 'o 3 stanje)) '-1 0)
+     (if (and (ista 4 5 stanje) (ima 'o 4 stanje)) '-1 0)
+     (if (and (ista 6 7 stanje) (ima 'x 6 stanje)) 1 0)
+     (if (and (ista 6 8 stanje) (ima 'x 6 stanje)) 1 0)
+     (if (and (ista 7 8 stanje) (ima 'x 7 stanje)) 1 0)
+     (if (and (ista 6 7 stanje) (ima 'o 6 stanje)) '-1 0)
+     (if (and (ista 6 8 stanje) (ima 'o 6 stanje)) '-1 0)
+     (if (and (ista 7 8 stanje) (ima 'o 7 stanje)) '-1 0)))
 
 (4connect)
